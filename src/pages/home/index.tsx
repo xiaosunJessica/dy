b94c82c0c6b2a63f9de534e5c7f3145e697ab88b
xiaokuoai <lovelysyq1991225@qq.com>
2025-02-25 16:08:50 +0800

    home

---
+++ src/pages/home/index.tsx
@@ -0,0 +1,15 @@
+import React from 'react';
+
+interface Props {
+  // props types
+}
+
+const ComponentName: React.FC<Props> = ({}) => {
+  return (
+    <div>
+      123
+    </div>
+  );
+};
+
+export default ComponentName;
